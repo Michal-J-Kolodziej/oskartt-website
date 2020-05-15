6efb41fa853b983f4645bfe568bc017a0a6967f4
Michał Kołodziej <asezrik@gmail.com>
2020-05-15 22:57:34 +0200

    .

--- website/oskartt-website/gatsby-node.js
+++ website/oskartt-website/gatsby-node.js
@@ -5,3 +5,5 @@
  */
 
 // You can delete this file if you're not using it
+console.log("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEELLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOO \n");
+console.log(process.env.GATSBY_SPOTIFY_API_KEY);
